a1e093fa2b598d74492254d7509ff20f360fde37
zincpool <zincpool@gmail.com>
2019-03-25 23:57:59 +0800

    init k-module

---
+++ src/main/java/com/z/kmodule/KModuleApplication.java
@@ -0,0 +1,13 @@
+package com.z.kmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KModuleApplication.class, args);
+	}
+
+}
